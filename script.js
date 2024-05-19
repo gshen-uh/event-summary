45ee715b6eab07712caba0a5a4e7c622fdc41642
gshen-uh <guoyang.shen@outlook.com>
2024-05-19 15:32:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,116 @@
-// Event listeners for buttons
-document.getElementById('generateEvents').addEventListener('click', generateEvents);
-document.getElementById('generatePlot').addEventListener('click', generatePlot);
+document.getElementById('generate-events').addEventListener('click', generateEvents);
+document.getElementById('generate-plot').addEventListener('click', generatePlot);
 
-// Function to generate events
 function generateEvents() {
-    console.log('Generate Events button clicked');
     const inputText = document.getElementById('activities').value.trim();
-    const eventsBox = document.getElementById('events');
     const extractedEvents = extractEvents(inputText);
 
-    const data = extractedEvents.map(event => {
-        const date = new Date(event[0]);
-        return `${date.toLocaleDateString()} ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })} ${event[1]}`;
-    }).join('\n');
-
-    eventsBox.value = data;
-}
-
-// Function to generate the plot
-function generatePlot() {
-    console.log('Generate Plot button clicked');
-    const eventsBox = document.getElementById('events');
-    const events = eventsBox.value.trim().split('\n');
-    const eventList = events.map(event => {
-        const [dateStr, timeStr, ...descriptionParts] = event.split(' ');
-        const dateTimeStr = `${dateStr} ${timeStr}`;
-        const dateTime = new Date(dateTimeStr);
-        const description = descriptionParts.join(' ');
-        return { date: dateTime, description: description };
+    let eventsDict = {};
+    extractedEvents.forEach(event => {
+        let eventDatetime = new Date(event[0]);
+        let eventDescription = event[1];
+        if (!eventsDict[eventDatetime] || eventDescription.length > eventsDict[eventDatetime].length) {
+            eventsDict[eventDatetime] = eventDescription;
+        }
     });
 
-    eventList.sort((a, b) => a.date - b.date);
+    let uniqueEvents = Object.entries(eventsDict).map(([dt, desc]) => [new Date(dt), desc]);
+    uniqueEvents.sort((a, b) => a[0] - b[0]);
 
-    const outputTable = document.getElementById('outputTable');
-    outputTable.innerHTML = generateEventTable(eventList.reverse());
-
-    generateEventPlot(eventList.reverse());
+    let data = uniqueEvents.map(event => `${event[0].toLocaleString()} ${event[1]}`).join('\n');
+    document.getElementById('events').value = data;
 }
 
-// Function to extract events from input text
+function generatePlot() {
+    const events = document.getElementById('events').value.trim().split('\n');
+    let eventList = events.map(event => {
+        let parts = event.split(' ', 2);
+        let dateTimeStr = `${parts[0]} ${parts[1]}`;
+        return [new Date(dateTimeStr), event.slice(parts[0].length + parts[1].length + 2)];
+    });
+
+    eventList.sort((a, b) => a[0] - b[0]);
+    const outputTable = document.getElementById('output-table');
+    const outputPlot = document.getElementById('output-plot');
+    
+    outputTable.innerHTML = generateTable(eventList);
+    generateTimeline(eventList);
+}
+
+function generateTable(eventList) {
+    let table = `<table border="1" style="width: 100%; border-collapse: collapse;">
+        <tr>
+            <th>Time</th>
+            <th>Event</th>
+        </tr>`;
+    eventList.reverse().forEach(event => {
+        let color = event[1].includes('Project Rework') ? 'red' : 'black';
+        table += `<tr>
+            <td>${event[0].toLocaleString()}</td>
+            <td style="color: ${color};">${event[1]}</td>
+        </tr>`;
+    });
+    table += '</table>';
+    return table;
+}
+
+function generateTimeline(eventList) {
+    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
+    const width = 960 - margin.left - margin.right;
+    const height = 500 - margin.top - margin.bottom;
+
+    d3.select("#output-plot").html('');
+
+    const svg = d3.select("#output-plot").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    const x = d3.scaleTime().range([0, width]);
+    const y = d3.scaleLinear().range([height, 0]);
+
+    x.domain(d3.extent(eventList, d => d[0]));
+    y.domain([0, eventList.length]);
+
+    const line = d3.line()
+        .x(d => x(d[0]))
+        .y((d, i) => y(i));
+
+    svg.append("path")
+        .data([eventList])
+        .attr("class", "line")
+        .attr("d", line)
+        .attr("stroke", "green")
+        .attr("fill", "none");
+
+    svg.selectAll("dot")
+        .data(eventList)
+        .enter().append("circle")
+        .attr("r", 5)
+        .attr("cx", d => x(d[0]))
+        .attr("cy", (d, i) => y(i))
+        .attr("fill", "green");
+
+    svg.selectAll("text")
+        .data(eventList)
+        .enter().append("text")
+        .attr("x", d => x(d[0]) + 5)
+        .attr("y", (d, i) => y(i))
+        .attr("dy", ".35em")
+        .attr("text-anchor", "start")
+        .text(d => d[1]);
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(x));
+
+    svg.append("g")
+        .call(d3.axisLeft(y).tickFormat('').tickSize(0));
+}
+
 function extractEvents(inputText) {
-    console.log('Extracting events from input text');
-    const lines = inputText.split('\n').map(line => line.trim());
+    const lines = inputText.trim().split('\n');
     const events = [];
     let currentDate = null;
     let currentTime = null;
@@ -50,115 +119,19 @@ function extractEvents(inputText) {
     const timePattern = /\d{1,2}:\d{2} (?:AM|PM)/;
 
     lines.forEach(line => {
+        line = line.trim();
+
         if (datePattern.test(line)) {
             currentDate = line;
         } else if (timePattern.test(line)) {
             currentTime = line;
-        } else if (line.toLowerCase().includes('created project')) {
-            const eventTime = `${currentDate} ${currentTime}`;
-            events.push([eventTime, 'Project created']);
-        } else if (line.toLowerCase().includes('changed the status to assigned to pilot')) {
-            const eventTime = `${currentDate} ${currentTime}`;
-            const pilotName = lines.slice(lines.indexOf(line) + 1).find(l => l.trim());
-            events.push([eventTime, `Assigned to Pilot (${pilotName})`]);
-        } else if (line.toLowerCase().includes('changed the status to project rework')) {
-            const eventTime = `${currentDate} ${currentTime}`;
-            const reasonTitleLine = lines.slice(lines.indexOf(line) + 1).find(l => l.toLowerCase().includes('title:'));
-            const reasonTitle = reasonTitleLine ? reasonTitleLine.split('Title:')[1].trim() : '';
-            events.push([eventTime, `Project Rework${reasonTitle ? ': ' + reasonTitle : ''}`]);
-        } else if (line.toLowerCase().includes('changed the status to')) {
-            const eventTime = `${currentDate} ${currentTime}`;
-            const status = line.split('changed the status to').pop().trim();
-            events.push([eventTime, status]);
+        } else {
+            if (currentDate && currentTime) {
+                const dateTimeStr = `${currentDate} ${currentTime}`;
+                events.push([dateTimeStr, line]);
+            }
         }
     });
 
-    console.log('Extracted events:', events);
     return events;
-}
-
-// Function to generate event table HTML
-function generateEventTable(events) {
-    const tableHeader = `
-        <table style='width:100%; border:1px solid black; border-collapse:collapse;'>
-            <tr style='border:1px solid black;'>
-                <th style='border:1px solid black; padding:4px; text-align:left;'>Time</th>
-                <th style='border:1px solid black; padding:4px; text-align:right;'>Event</th>
-            </tr>
-    `;
-
-    const tableRows = events.map(event => {
-        const color = event.description.includes('Project Rework') ? 'red' : 'black';
-        return `
-            <tr style='border:1px solid black;'>
-                <td style='border:1px solid black; padding:4px; text-align:left;'>${event.date.toISOString().slice(0, 19).replace('T', ' ')}</td>
-                <td style='border:1px solid black; padding:4px; text-align:right; color:${color};'>${event.description}</td>
-            </tr>
-        `;
-    }).join('');
-
-    return tableHeader + tableRows + '</table>';
-}
-
-// Function to generate event plot using D3.js
-function generateEventPlot(events) {
-    const outputPlot = document.getElementById('outputPlot');
-    outputPlot.innerHTML = '';
-
-    const width = 800;
-    const height = 400;
-
-    const svg = d3.select('#outputPlot')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height);
-
-    const margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    const plotWidth = width - margin.left - margin.right;
-    const plotHeight = height - margin.top - margin.bottom;
-
-    const x = d3.scaleTime()
-        .domain(d3.extent(events, d => d.date))
-        .range([0, plotWidth]);
-
-    const y = d3.scaleLinear()
-        .domain([0, events.length - 1])
-        .range([0, plotHeight]);
-
-    const line = d3.line()
-        .x(d => x(d.date))
-        .y((d, i) => y(i));
-
-    svg.append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`)
-        .append('path')
-        .datum(events)
-        .attr('fill', 'none')
-        .attr('stroke', 'green')
-        .attr('stroke-width', 1.5)
-        .attr('d', line);
-
-    svg.append('g')
-        .attr('transform', `translate(${margin.left},${plotHeight + margin.top})`)
-        .call(d3.axisBottom(x).ticks(d3.timeDay.every(1)).tickFormat(d3.timeFormat('%b %d')))
-        .selectAll('text')
-        .attr('transform', 'rotate(-45)')
-        .style('text-anchor', 'end');
-
-    svg.append('g')
-        .attr('transform', `translate(${margin.left},${margin.top})`)
-        .selectAll('.dot')
-        .data(events)
-        .enter().append('circle')
-        .attr('cx', d => x(d.date))
-        .attr('cy', (d, i) => y(i))
-        .attr('r', 5)
-        .attr('fill', 'green');
-
-    svg.append('text')
-        .attr('transform', `translate(${width / 2},${margin.top / 2})`)
-        .attr('text-anchor', 'middle')
-        .attr('font-size', '16px')
-        .attr('font-weight', 'bold')
-        .text(document.getElementById('projectId').value);
 }
